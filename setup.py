2e34b37db92fc75caefc09e3dc32251111208688
Thijs Triemstra <thijs@collab.com>
2007-10-19 05:58:56 +0000

    Adding license and setup files.
    git-svn-id: https://svn.pyamf.org/trunk@3 2dde4cc4-cf3c-0410-b1a3-a9b8ff274da5

---
+++ setup.py
@@ -0,0 +1,46 @@
+#
+# Copyright (c) 2007 The PyAMF Project. All rights reserved.
+# 
+# Arnar Birgisson
+# Thijs Triemstra
+# 
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+from ez_setup import use_setuptools
+
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(name = "PyAMF",
+    version = "0.0.1",
+    description = "AMF encoder and decoder for Python",
+    url = "http://dev.collab.com/pyamf",
+    packages = ["pyamf"],
+    test_suite = "pyamf.tests.suite",
+    license = "MIT License",
+    classifiers = [
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ])
