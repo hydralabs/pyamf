09d4ed326165ee52c544c4cfa4b2611b02c79b69
Nick Joyce <nick@boxdesign.co.uk>
2007-11-07 15:24:06 +0000

    Moved messaging related classes to their own module
    git-svn-id: https://svn.pyamf.org/trunk@155 2dde4cc4-cf3c-0410-b1a3-a9b8ff274da5

---
+++ pyamf/messaging.py
@@ -0,0 +1,98 @@
+# -*- encoding: utf8 -*-
+#
+# Copyright (c) 2007 The PyAMF Project. All rights reserved.
+# 
+# Arnar Birgisson
+# Thijs Triemstra
+# Nick Joyce
+# 
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+Flex Messaging Implementation
+"""
+
+import pyamf
+
+class AbstractMessage(object):
+    """
+    Base class for all Flex compatibility messages.
+    """
+    data = None
+    clientId = None
+    destination = None
+    headers = []
+    messageId = None
+    timeToLive = None
+    timestamp = None
+    
+    def __repr__(self):
+        m = '<%s ' % self.__class__.__name__
+
+        for k, v in self.__dict__.iteritems():
+            m += ' %s=%s' % (k, v)
+
+        return m + " />"
+
+class AsyncMessage(AbstractMessage):
+    """
+    Base class for for asynchronous Flex compatibility messages.
+    """
+    correlationId = None
+
+class AcknowledgeMessage(AsyncMessage):
+    """
+    Flex compatibility message that is returned to the client.
+    """
+    pass
+
+class CommandMessage(AsyncMessage):
+    """
+    Command message as sent by the C{<mx:RemoteObject>} MXML tag.
+
+    Reference: U{http://livedocs.adobe.com/flex/2/langref/mx/rpc/remoting/mxml/RemoteObject.html}
+    """
+    operation = None
+    messageRefType = None
+
+class ErrorMessage(AbstractMessage):
+    """
+    Flex error message to be returned to the client.
+    """
+    extendedData = {}
+    faultCode = None
+    faultDetail = None
+    faultString = None
+    rootCause = {}
+
+class RemotingMessage(AbstractMessage):
+    """
+    Flex compatibility message that is sent by the C{<mx:RemoteObject>} MXML tag.
+    """
+    #: Name of the method to be called.
+    operation = None
+    #: Name of the service to be called
+    #" including package name.
+    source = None
+
+pyamf.register_class(RemotingMessage, 'flex.messaging.messages.RemotingMessage')
+pyamf.register_class(ErrorMessage, 'flex.messaging.messages.ErrorMessage')
+pyamf.register_class(CommandMessage, 'flex.messaging.messages.CommandMessage')
+pyamf.register_class(AcknowledgeMessage, 'flex.messaging.messages.AcknowledgeMessage')
