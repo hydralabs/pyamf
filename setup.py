7d6c527b3459e83bf0c545aae808e84e298de1a1
Nick Joyce <nick@boxdesign.co.uk>
2008-04-14 14:55:41 +0000

    Merging source:pyamf/branches/setup-desc-251
    git-svn-id: https://svn.pyamf.org/pyamf/trunk@1194 2dde4cc4-cf3c-0410-b1a3-a9b8ff274da5

--- setup.py
+++ setup.py
@@ -32,25 +32,6 @@ install_requires = ["fpconst>=0.7.2", "Importing>=1.9.2"]
 if sys.version_info < (2, 5):
     install_requires.extend(["elementtree >= 1.2.6", "uuid>=1.30"])
 
-long_desc = """\
-PyAMF provides Action Message Format (AMF) support for Python that is
-compatible with the Flash Player.
-
-The Adobe Integrated Runtime and Flash Player use AMF to communicate
-between an application and a remote server. AMF encodes remote procedure
-calls (RPC) into a compact binary representation that can be transferred
-over HTTP/HTTPS or the RTMP/RTMPS protocol. Objects and data values are
-serialized into this binary format, which increases performance,
-allowing applications to load data up to 10 times faster than with
-text-based formats such as XML or SOAP.
-
-AMF 3, the default serialization for ActionScript 3.0, provides various
-advantages over AMF0, which is used for ActionScript 1.0 and 2.0. AMF 3
-sends data over the network more efficiently than AMF 0. AMF 3 supports
-sending int and uint objects as integers and supports data types that are
-available only in ActionScript 3.0, such as ByteArray, ArrayCollection,
-and IExternalizable."""
-
 keyw = """\
 amf amf0 amf3 flex flash remoting rpc http flashplayer air bytearray
 objectproxy arraycollection recordset actionscript decoder encoder
@@ -59,7 +40,7 @@ gateway"""
 setup(name = "PyAMF",
     version = "0.3",
     description = "AMF support for Python",
-    long_description = long_desc,
+    long_description = open('README.txt', 'rt').read(),
     url = "http://pyamf.org",
     author = "The PyAMF Project",
     author_email = "dev@pyamf.org",
