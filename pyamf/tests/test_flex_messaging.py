83410a67ac5cebe0a03899c816bd37eb47d9641e
Nick Joyce <nick@boxdesign.co.uk>
2008-05-13 18:41:51 +0000

    Adding tests for checking unicode repr for messaging.AbstractMessage
    git-svn-id: https://svn.pyamf.org/pyamf/trunk@1374 2dde4cc4-cf3c-0410-b1a3-a9b8ff274da5

---
+++ pyamf/tests/test_flex_messaging.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2007-2008 The PyAMF Project.
+# See LICENSE for details.
+
+"""
+Flex Messaging compatibility tests.
+
+@author: U{Nick Joyce<mailto:nick@boxdesign.co.uk>}
+@since: 0.3.2
+"""
+
+import unittest
+
+from pyamf.flex import messaging
+
+class AbstractMessageTestCase(unittest.TestCase):
+    def test_repr(self):
+        a = messaging.AbstractMessage()
+
+        a.body = u'é,è'
+
+        print repr(a)
+
+def suite():
+    suite = unittest.TestSuite()
+
+    suite.addTest(unittest.makeSuite(AbstractMessageTestCase))
+
+    return suite
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
