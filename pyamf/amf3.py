bdba0e3cfc1402776e7cbe239d185f72d32d2a70
Thijs Triemstra <thijs@collab.com>
2009-03-13 14:30:41 +0000

    Update for link to latest AMF3 spec
    git-svn-id: https://svn.pyamf.org/pyamf/trunk@2259 2dde4cc4-cf3c-0410-b1a3-a9b8ff274da5

--- pyamf/amf3.py
+++ pyamf/amf3.py
@@ -14,7 +14,7 @@ supports data types that are available only in ActionScript 3.0, such as
 L{ByteArray} and L{ArrayCollection}.
 
 @see: U{Official AMF3 Specification in English (external)
-<http://opensource.adobe.com/wiki/download/attachments/1114283/amf3_spec_121207.pdf>}
+<http://opensource.adobe.com/wiki/download/attachments/1114283/amf3_spec_05_05_08.pdf>}
 @see: U{Official AMF3 Specification in Japanese (external)
 <http://opensource.adobe.com/wiki/download/attachments/1114283/JP_amf3_spec_121207.pdf>}
 @see: U{AMF3 documentation on OSFlash (external)
