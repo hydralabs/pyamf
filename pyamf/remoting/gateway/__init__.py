95b8a28f29be9726fd6b070440ad2829158b0641
Thijs Triemstra <thijs@collab.com>
2009-06-14 10:08:34 +0000

    Merge jython-server-name-541: Replaced hardcoded Python implementation title in p.r.gateway.SERVER_NAME with platform.python_implementation()
    Author: thijs
    Reviewer: nick
    Fixes: #541
    
    
    git-svn-id: https://svn.pyamf.org/pyamf/trunk@2519 2dde4cc4-cf3c-0410-b1a3-a9b8ff274da5

--- pyamf/remoting/gateway/__init__.py
+++ pyamf/remoting/gateway/__init__.py
@@ -13,8 +13,14 @@ import types
 import pyamf
 from pyamf import remoting, util
 
-SERVER_NAME = 'PyAMF/%s Python/%s' % (
-    '.'.join(map(lambda x: str(x), pyamf.__version__)),
+try:
+    from platform import python_implementation
+    impl = python_implementation()
+except ImportError:
+    impl = 'Python'
+    
+SERVER_NAME = 'PyAMF/%s %s/%s' % (
+    '.'.join(map(lambda x: str(x), pyamf.__version__)), impl,
     '.'.join(map(lambda x: str(x), sys.version_info[0:3]))
 )
 
