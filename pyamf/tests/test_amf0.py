7a13ce38c5f4ec453887d9b7876342c345c91df1
Nick Joyce <nick@boxdesign.co.uk>
2009-06-15 07:08:17 +0000

    Merged decode-position-573: If a call to Decoder.readElement results in an IOError exception, the original position of the stream will be restored
    Fixes: #573
    Author: nick
    Reviewer: thijs
    
    
    git-svn-id: https://svn.pyamf.org/pyamf/trunk@2523 2dde4cc4-cf3c-0410-b1a3-a9b8ff274da5

--- pyamf/tests/test_amf0.py
+++ pyamf/tests/test_amf0.py
@@ -888,6 +888,22 @@ class DecoderTestCase(ClassCacheClearingTestCase):
         self.assertEquals(foo.family_name, 'Doe')
         self.assertEquals(foo.given_name, 'Jane')
 
+    def test_ioerror_buffer_position(self):
+        """
+        Test to ensure that if an IOError is raised by `readElement` that
+        the original position of the stream is restored.
+        """
+        bytes = pyamf.encode(u'foo', [1, 2, 3], encoding=pyamf.AMF0).getvalue()
+
+        self.buf.write(bytes[:-1])
+        self.buf.seek(0)
+
+        self.decoder.readElement()
+        self.assertEquals(self.buf.tell(), 6)
+
+        self.assertRaises(IOError, self.decoder.readElement)
+        self.assertEquals(self.buf.tell(), 6)
+
 
 class HelperTestCase(unittest.TestCase):
     def test_encode(self):
