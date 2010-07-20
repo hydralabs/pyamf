d3bd6aa7a6a5c9ef25b0873439fa1f04b60e70c3
Nick Joyce <nick@boxdesign.co.uk>
2010-07-20 17:14:16 +1000

    Move all xml util funcs to their own module - pyamf.util.xml

--- pyamf/util/__init__.py
+++ pyamf/util/__init__.py
@@ -16,6 +16,7 @@ import inspect
 
 import pyamf
 from pyamf import python
+from pyamf.util.xml import set_xml_type, find_xml_lib, is_xml_type
 
 try:
     from cpyamf.util import BufferedByteStream
@@ -23,60 +24,9 @@ except ImportError:
     from pyamf.util.pure import BufferedByteStream
 
 
-ETREE_MODULES = [
-    'xml.etree.cElementTree',
-    'cElementTree',
-    'xml.etree.ElementTree',
-    'elementtree.ElementTree'
-]
-
-#: XML types.
-xml_types = None
-xml_modules = None
-
-ET = None
 #: On some Python versions retrieving a negative timestamp, like
 #: C{datetime.datetime.utcfromtimestamp(-31536000.0)} is broken.
 negative_timestamp_broken = False
-
-
-def find_xml_lib():
-    """
-    Run through a predefined order looking through the various C{ElementTree}
-    implementations so that any type can be encoded but PyAMF will return
-    elements as the first implementation found.
-
-    We work through the C implementations first - then the pure Python
-    versions. The downside to this is that a possible of three libraries will
-    be loaded into memory that are not used but the libs are small
-    (relatively) and the flexibility that this gives seems to outweigh the
-    cost. Time will tell.
-
-    @since: 0.4
-    """
-    types = []
-    modules = []
-
-    for mod in ETREE_MODULES:
-        try:
-            etree = get_module(mod)
-        except ImportError:
-            continue
-
-        modules.append(etree)
-        e = etree.Element('foo')
-
-        try:
-            types.append(e.__class__)
-        except AttributeError:
-            types.append(type(e))
-
-    # hack for jython
-    for x in types[:]:
-        if x.__name__ == 'instance':
-            types.remove(x)
-
-    return tuple(types), tuple(modules)
 
 
 def get_timestamp(d):
@@ -254,44 +204,6 @@ def get_module(mod_name):
         mod = getattr(mod, comp)
 
     return mod
-
-
-def set_xml_type(t):
-    """
-    Sets the default type that PyAMF will use to construct XML objects,
-    supplying the stringified XML to the caller.
-    """
-    global xml_modules, ET
-
-    if xml_modules is None:
-        xml_modules = (t,)
-    else:
-        types = set(xml_modules)
-        types.update([t])
-
-        xml_modules = tuple(types)
-
-    ET = t
-
-
-def is_xml_type(t):
-    """
-    Determines if the type object is a valid XML type.
-
-    If L{xml_types} is not populated then it will call L{find_xml_lib}.
-    """
-    global xml_types, xml_modules, ET
-
-    if xml_types is None:
-        xml_types, xml_modules = find_xml_lib()
-
-    if ET is None:
-        try:
-            ET = xml_modules[0]
-        except IndexError:
-            return False
-
-    return isinstance(t, xml_types)
 
 
 try:
