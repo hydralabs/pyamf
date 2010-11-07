b25c38d8866a167a7c86c26023fea171cdf14289
Thijs Triemstra <info@collab.nl>
2010-11-08 06:47:50 +0800

    remove trailing 0

--- pyamf/__init__.py
+++ pyamf/__init__.py
@@ -30,7 +30,7 @@ __all__ = [
 ]
 
 #: PyAMF version number.
-__version__ = version = v.Version(0, 6, 0)
+__version__ = version = v.Version(0, 6)
 
 #: Class alias mapping support. Contains two types of keys: The string alias
 #: related to the class and the class object itself. Both point to the linked
