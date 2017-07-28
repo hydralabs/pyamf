e79150caaf6034d317918d7651bab535566e110a
Yohan <yohan@buzzni.com>
2017-07-28 15:21:53 +0900

    Fix wrong testcase

--- pyamf/tests/test_amf3.py
+++ pyamf/tests/test_amf3.py
@@ -286,10 +286,8 @@ class EncoderTestCase(ClassCacheClearingTestCase, EncoderMixIn):
             ),
             b'\x01'
         )
-        self.assertEncoded({12: True, 42: "Testing"}, (b'\n\x0b', (
-            b'\x01\x0542\x06\x0fTesting',
-            b'\x0512\x03\x01'
-        )))
+        self.assertEncoded({12: True, 42: "Testing"},
+                           b'\n\x0b\x01\x0542\x06\x0fTesting\x0512\x03\x01')
 
     def test_boolean(self):
         self.assertEncoded(True, b'\x03')
